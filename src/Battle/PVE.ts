922fe4f39335be017cb4619b828ef63d0c2958f0
VictorSilva27 <victoradaosilva2@gmail.com>
2022-11-19 13:15:57 -0300

    Requisito 12

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,20 +1,24 @@
-import Fighter from '../Fighter';
-import Monster from '../Monster';
+import Fighter, { SimpleFighter } from '../Fighter';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
   constructor(
-    private lutador: Fighter,
-    private arrayMonster: Monster[],
+    private lutador: Fighter | SimpleFighter,
+    private arrayMonster: SimpleFighter[],
   ) {
     super(lutador);
     super.fight();
   }
 
   fight(): number {
-    while (this.lutador.lifePoints > 0 && this.arrayMonster[0].lifePoints > 0) {
-    //   this.lutador.attack();
-      this.arrayMonster[0].attack(this.lutador);
+    let monster = 0;
+    while (monster <= this.arrayMonster.length - 1) {
+      while (this.lutador.lifePoints > 0
+            && this.arrayMonster[monster].lifePoints > 0) {
+        this.lutador.attack(this.arrayMonster[monster]);
+        this.arrayMonster[monster].attack(this.lutador);
+      }
+      monster += 1;
     }
 
     return this.lutador.lifePoints === -1 ? -1 : 1;
